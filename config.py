ab21692938053678afa466e57d9c30ffbe07b36f
𝔓𝔯𝔢𝔪𝔦𝔲𝔪 𝔇𝔲𝔫𝔤𝔢𝔬𝔫 <ejiromeshack253@gmail.com>
2025-06-22 15:34:45 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ ADMINS = [5650788149, 548105255]
 
 TOKEN = "8186651135:AAEq_xB2dzdxMpPIGJljdLHmsBh9oXqWDZc"
 
+CHANNEL_USERNAME = "gouglenetwork"  # no @ symbol
+
 WEBHOOK_URL = "https://patrickeded.onrender.com"
 
 # Cooldowns (in seconds)
